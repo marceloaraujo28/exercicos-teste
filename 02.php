dd89a043472109c9cdb3bedd577b5894aab4c26f
Marcelo <marcelo_araujo28@hotmail.com>
2022-01-10 20:55:47 -0300

    Exercice 2 Format

--- 02.php
+++ 02.php
@@ -1,26 +1,21 @@
-<?php 
-    
-    function Primos($inicial, $final){
+<?php
 
-        $list = [];
-
-        for($i = $inicial+1; $i < $final; $i++){
-            $counter = 0; 
-
-                for($j=1;$j<=$i;$j++){ 
-                    if($i % $j==0){ 
-                        $counter++;
-                     }
-                } 
-            if($counter==2){
-                    $list[] = $i;
-             }
+function Primos($inicial, $final)
+{
+    $list = [];
+    for ($i = $inicial + 1; $i < $final; $i++) {
+        $counter = 0;
+        for ($j = 1; $j <= $i; $j++) {
+            if ($i % $j == 0) {
+                $counter++;
+            }
         }
-
-        print_r($list);
+        if ($counter == 2) {
+            $list[] = $i;
+        }
     }
 
-Primos(10,29)
+    print_r($list);
+}
 
-
-?>
+Primos(10, 29);
